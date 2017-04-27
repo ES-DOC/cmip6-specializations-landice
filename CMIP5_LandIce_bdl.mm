2faf713b34c40eec28c0d0fdb34173b856641f75
David Hassell <d.c.hassell@reading.ac.uk>
2017-04-27 09:04:10 +0100

    CMIP5 mind map

---
+++ CMIP5_LandIce_bdl.mm
@@ -0,0 +1,149 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  
+  
+
+  
+
+
+  <head>
+    <title>
+      LandIce_bdl.mm in controlled_vocabularies/tags/cmip5_vn1.0/Software
+     – METAFOR Task Tracker
+    </title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <link rel="search" href="/trac/search" />
+        <link rel="help" href="/trac/wiki/TracGuide" />
+        <link rel="alternate" href="/trac/browser/controlled_vocabularies/tags/cmip5_vn1.0/Software/LandIce_bdl.mm?format=txt" type="text/plain" title="Plain Text" /><link rel="alternate" href="/trac/export/3812/controlled_vocabularies/tags/cmip5_vn1.0/Software/LandIce_bdl.mm" type="application/x-freemind; charset=iso-8859-15" title="Original Format" />
+        <link rel="tracwysiwyg.base" href="/trac" />
+        <link rel="start" href="/trac/wiki" />
+        <link rel="stylesheet" href="/trac/chrome/common/css/trac.css" type="text/css" /><link rel="stylesheet" href="/trac/chrome/common/css/code.css" type="text/css" /><link rel="stylesheet" href="/trac/chrome/common/css/browser.css" type="text/css" /><link rel="stylesheet" href="/trac/chrome/tracwysiwyg/wysiwyg.css" type="text/css" />
+        <link rel="tracwysiwyg.stylesheet" href="/trac/chrome/common/css/trac.css" /><link rel="tracwysiwyg.stylesheet" href="/trac/chrome/tracwysiwyg/editor.css" />
+        <link rel="shortcut icon" href="/trac/chrome/site/trac.ico" type="image/x-icon" />
+        <link rel="icon" href="/trac/chrome/site/trac.ico" type="image/x-icon" />
+      <link type="application/opensearchdescription+xml" rel="search" href="/trac/search/opensearch" title="Search METAFOR Task Tracker" />
+    <script type="text/javascript" src="/trac/chrome/common/js/jquery.js"></script><script type="text/javascript" src="/trac/chrome/common/js/babel.js"></script><script type="text/javascript" src="/trac/chrome/common/js/trac.js"></script><script type="text/javascript" src="/trac/chrome/common/js/search.js"></script><script type="text/javascript" src="/trac/chrome/tracwysiwyg/wysiwyg.js"></script><script type="text/javascript" src="/trac/chrome/tracwysiwyg/wysiwyg-load.js"></script>
+    <!--[if lt IE 7]>
+    <script type="text/javascript" src="/trac/chrome/common/js/ie_pre7_hacks.js"></script>
+    <![endif]-->
+    <script type="text/javascript" src="/trac/chrome/common/js/folding.js"></script>
+    <script type="text/javascript">
+      jQuery(document).ready(function($) {
+        $(".trac-toggledeleted").show().click(function() {
+                  $(this).siblings().find(".trac-deleted").toggle();
+                  return false;
+        }).click();
+        $("#jumploc input").hide();
+        $("#jumploc select").change(function () {
+          this.parentNode.parentNode.submit();
+        });
+          $('#preview table.code').enableCollapsibleColumns($('#preview table.code thead th.content'));
+      });
+    </script>
+  </head>
+  <body>
+    <div id="siteheader">
+    </div>
+    <div id="banner">
+      <div id="header">
+        <a id="logo" href="http://metaforclimate.eu/"><img src="/trac/chrome/common/metafor_logo_banner2.png" alt="METAFOR Task Tracker" /></a>
+      </div>
+      <form id="search" action="/trac/search" method="get">
+        <div>
+          <label for="proj-search">Search:</label>
+          <input type="text" id="proj-search" name="q" size="18" value="" />
+          <input type="submit" value="Search" />
+        </div>
+      </form>
+      <div id="metanav" class="nav">
+    <ul>
+      <li class="first"><a href="/trac/login">Login</a></li><li><a href="/trac/wiki/TracGuide">Help/Guide</a></li><li><a href="/trac/about">About Trac</a></li><li class="last"><a href="/trac/prefs">Preferences</a></li>
+    </ul>
+  </div>
+    </div>
+    <div id="mainnav" class="nav">
+    <ul>
+      <li class="first"><a href="/trac/wiki">Wiki</a></li><li><a href="/trac/timeline">Timeline</a></li><li><a href="/trac/roadmap">Roadmap</a></li><li class="active"><a href="/trac/browser">Browse Source</a></li><li><a href="/trac/report">View Tickets</a></li><li><a href="/trac/search">Search</a></li><li class="last"><a href="/trac/wiki/metaforHelp">Help</a></li>
+    </ul>
+  </div>
+    <div id="main">
+      <div id="ctxtnav" class="nav">
+        <h2>Context Navigation</h2>
+          <ul>
+              <li class="first"><a href="/trac/browser/controlled_vocabularies/branches/cmip5/Software/LandIce_bdl.mm?annotate=blame&amp;rev=3698" title="Annotate each line with the last changed revision (this can be time consuming...)">Annotate</a></li><li class="last"><a href="/trac/log/controlled_vocabularies/tags/cmip5_vn1.0/Software/LandIce_bdl.mm">Revision Log</a></li>
+          </ul>
+        <hr />
+      </div>
+    <div id="content" class="browser">
+          <h1>
+<a class="pathentry first" href="/trac/browser?order=name" title="Go to repository root">source:</a>
+<a class="pathentry" href="/trac/browser/controlled_vocabularies?order=name" title="View controlled_vocabularies">controlled_vocabularies</a><span class="pathentry sep">/</span><a class="pathentry" href="/trac/browser/controlled_vocabularies/tags?order=name" title="View tags">tags</a><span class="pathentry sep">/</span><a class="pathentry" href="/trac/browser/controlled_vocabularies/tags/cmip5_vn1.0?order=name" title="View cmip5_vn1.0">cmip5_vn1.0</a><span class="pathentry sep">/</span><a class="pathentry" href="/trac/browser/controlled_vocabularies/tags/cmip5_vn1.0/Software?order=name" title="View Software">Software</a><span class="pathentry sep">/</span><a class="pathentry" href="/trac/browser/controlled_vocabularies/tags/cmip5_vn1.0/Software/LandIce_bdl.mm?order=name" title="View LandIce_bdl.mm">LandIce_bdl.mm</a>
+<span class="pathentry sep">@</span>
+  <a class="pathentry" href="/trac/changeset/3698" title="View changeset 3698">3698</a>
+<br style="clear: both" />
+</h1>
+<font size="-1">Subversion URL:</font> <font size="-2"><a href="http://proj.badc.rl.ac.uk/svn/metafor/controlled_vocabularies/tags/cmip5_vn1.0/Software/LandIce_bdl.mm" title="View path using apache browser">http://proj.badc.rl.ac.uk/svn/metafor/controlled_vocabularies/tags/cmip5_vn1.0/Software/LandIce_bdl.mm</a></font>
+        <div id="jumprev">
+          <form action="" method="get">
+            <div>
+              <label for="rev">
+                View revision:</label>
+              <input type="text" id="rev" name="rev" size="6" />
+            </div>
+          </form>
+        </div>
+      <table id="info" summary="Revision info">
+        <tr>
+          <th scope="col">Revision <a href="/trac/changeset/3698">3698</a>,
+            <span title="34824 bytes">34.0 KB</span>
+            checked in by allyn, <a class="timeline" href="/trac/timeline?from=2012-11-14T15%3A51%3A15Z&amp;precision=second" title="2012-11-14T15:51:15Z in Timeline">4 years</a> ago
+            (<a href="/trac/changeset/3698/controlled_vocabularies/branches/cmip5/Software/LandIce_bdl.mm">diff</a>)</th>
+        </tr>
+        <tr>
+          <td class="message searchable">
+              <p>
+modified directory structure<br />
+</p>
+          </td>
+        </tr>
+      </table>
+      <div id="preview" class="searchable">
+        
+  <div style="border: 1px solid #cccccc; height: 600px; width: 100%;" id="flashcontent"><embed src="/trac/chrome/swf/visorFreemind.swf" flashvars="openUrl=_blank&amp;min_alpha_buttons=20&amp;initLoadFile=/trac/export/3812/controlled_vocabularies/tags/cmip5_vn1.0/Software/LandIce_bdl.mm&amp;max_alpha_buttons=100&amp;defaultToolTipWordWrap=200&amp;startCollapsedToLevel=3&amp;baseImagePath=/trac/export/3812/controlled_vocabularies/tags/cmip5_vn1.0/Software/" bgcolor="#ffffff" quality="high" align="middle" height="100%" width="100%" type="application/x-shockwave-flash" id="visorFreeMind"/></div>
+
+      </div>
+      <div id="help"><strong>Note:</strong> See <a href="/trac/wiki/TracBrowser">TracBrowser</a>
+        for help on using the repository browser.</div>
+      <div id="anydiff">
+        <form action="/trac/diff" method="get">
+          <div class="buttons">
+            <input type="hidden" name="new_path" value="/controlled_vocabularies/tags/cmip5_vn1.0/Software/LandIce_bdl.mm" />
+            <input type="hidden" name="old_path" value="/controlled_vocabularies/tags/cmip5_vn1.0/Software/LandIce_bdl.mm" />
+            <input type="hidden" name="new_rev" />
+            <input type="hidden" name="old_rev" />
+            <input type="submit" value="View changes..." title="Select paths and revs for Diff" />
+          </div>
+        </form>
+      </div>
+    </div>
+    <div id="altlinks">
+      <h3>Download in other formats:</h3>
+      <ul>
+        <li class="first">
+          <a rel="nofollow" href="/trac/browser/controlled_vocabularies/tags/cmip5_vn1.0/Software/LandIce_bdl.mm?format=txt">Plain Text</a>
+        </li><li class="last">
+          <a rel="nofollow" href="/trac/export/3812/controlled_vocabularies/tags/cmip5_vn1.0/Software/LandIce_bdl.mm">Original Format</a>
+        </li>
+      </ul>
+    </div>
+    </div>
+    <div id="footer" lang="en" xml:lang="en"><hr />
+      <a id="tracpowered" href="http://trac.edgewall.org/"><img src="/trac/chrome/common/trac_logo_mini.png" height="30" width="107" alt="Trac Powered" /></a>
+      <p class="left">Powered by <a href="/trac/about"><strong>Trac 0.12.2</strong></a><br />
+        By <a href="http://www.edgewall.org/">Edgewall Software</a>.</p>
+      <p class="right">Visit the Trac open source project at<br /><a href="http://trac.edgewall.org/">http://trac.edgewall.org/</a></p>
+    </div>
+    <div id="sitefooter">
+    </div>
+  </body>
+</html>
